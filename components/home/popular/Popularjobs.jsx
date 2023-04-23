fcd8d16276bb087edb1b3817106378634bc617be
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-04-23 13:51:15 +0530

    changes

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -5,36 +5,36 @@ import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
-
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
-  const isLoading = false;
-  const error = false;
+
   const router = useRouter();
+  const { data, isLoading, error } = useFetch( 'search', { query:'React Developer', num_pages:1 });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular Jobs</Text>
+    <View style = {styles.container}>
+      <View style = {styles.header}>
+        <Text style = {styles.headerTitle}>Popular Jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>show all</Text>
+          <Text style = {styles.headerBtn}>show all</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.cardsContainer}>
+      <View style = {styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" colors={COLORS.primary}/>          
+          <ActivityIndicator size = "large" colors = {COLORS.primary}/>          
         ):error ? (
           <Text>Something went wrong</Text>
         ):(
           <FlatList 
-            data={[1,2,3,4,5,6,7,8,9]}
-            renderItem={({item})=>(
+            data={ [1,2,3,4,5,6,7,8,9] }
+            renderItem={ ({item}) => (
              <PopularJobCard
                 item = {item}
              /> 
             )}
-            keyExtractor={item => item?.job_id}
-            contentContainerStyle={{columnGap:SIZES.medium}}
+            keyExtractor = { item => item?.job_id }
+            contentContainerStyle = {{ columnGap:SIZES.medium }}
             horizontal  
           />
         )}
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,17 +1,51 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+// import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
     const [data,setData] = useState([]);
     const [isLoading,setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const axios = require("axios");
+
     const options = {
         method: 'GET',
-        url: 'https://jsearch.p.rapidapi.com/search',
-        params: {query:'Python developer in Texas, USA', page: '1', num_pages: '1'},
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: { ...query },
         headers: {
-            //rest tommorow       
+            'X-RapidAPI-Key': '2556947d84mshd1800168fa179b5p172e6cjsnd86a967089f9',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch(error) {
+            setError(error);
+            alert("There is an error");
+        } finally {
+            setIsLoading(false);
         }
     }
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+
 }
+
+export default useFetch;
